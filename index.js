63635175ea24a9ed91f1da94d85056c2ad54fc1e
vishal <vishalsingh979896@gmail.com>
2020-04-19 02:55:37 +0530

    add eventListener

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+/*
+Basic way of calling click event function
+function clickHandle(){
+  alert("i got clicked");
+}
+// clickHandle instead of clickHandle(), because clickHandle() runs automatically everytime document loaded.
+document.querySelector("button").addEventListener("click",clickHandle);
+*/
 
+/*
+only applicable for first button
+document.querySelector(".drum").addEventListener("click",function(){
+  alert("I got clicked");
+});
+*/

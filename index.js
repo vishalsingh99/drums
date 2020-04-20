97b17302e3b2b20d05136a6783304e0d55e462c3
vishal <vishalsingh979896@gmail.com>
2020-04-21 02:24:17 +0530

    added sound for each click

--- index.js
+++ index.js
@@ -26,6 +26,52 @@ audio.play();
 var button=document.querySelectorAll(".drum").length;
 for (var i=0;i<button;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-     console.log(this);
+     //console.log(this);
+     //console.log(this.innerHTML);
+
+    var buttonClicked= this.innerHTML;
+     switch (buttonClicked) {
+       case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+         break;
+       case "a":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+           break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+             break;
+        case "d":
+              var tom4 = new Audio("sounds/tom-4.mp3");
+              tom4.play();
+               break;
+
+        case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                 break;
+        case "k":
+                  var snare = new Audio("sounds/snare.mp3");
+                  snare.play();
+                   break;
+        case "l":
+               var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+       default: console.log(this.innerHTML);
+
+     }
+
+
+
+
+
+
+
+
+
   });
 }
